911a0b66fa0e060244699109511d01cbc946f503
Alex Antonov <alex@antonov.ws>
2010-08-08 01:36:07 +0000

    Initial code checkin

---
+++ src/main/groovy/ws/antonov/gradle/plugins/protobuf/ProtobufCompile.groovy
@@ -0,0 +1,37 @@
+package ws.antonov.gradle.plugins.protobuf
+
+import org.gradle.api.InvalidUserDataException
+import org.gradle.api.logging.LogLevel
+import org.gradle.util.GUtil
+import org.gradle.api.tasks.compile.Compile
+import org.gradle.api.tasks.compile.AbstractCompile
+
+public class ProtobufCompile extends AbstractCompile {
+
+    public String getProtocPath() {
+        return null
+    }
+
+    protected void compile() {
+        //println "Compiling protos..."
+        //println "${sourceSets.main.java.srcDirs}"
+        //println project.getConvention().getPlugin(JavaPluginConvention.class).getSourceSets().getByName(SourceSet.MAIN_SOURCE_SET_NAME).protobuf.class
+        getDestinationDir().mkdir()
+        def dirs = GUtil.join(getDefaultSource().srcDirs, " -I")
+        //println dirs
+        def files = GUtil.join(getDefaultSource().getFiles(), " ")
+        def cmd = "${getProtocPath()} -I${dirs} --java_out=${getDestinationDir()} ${files}"
+        logger.log(LogLevel.INFO, cmd)
+        Process result = cmd.execute()
+        result.waitFor()
+        def sbout = new StringBuffer()
+        def sberr = new StringBuffer()
+        result.consumeProcessOutput(sbout, sberr)
+        if (result.exitValue() == 0) {
+            logger.log(LogLevel.INFO, sbout.toString())
+        } else {
+            //logger.log(LogLevel.ERROR, sberr.toString())
+            throw new InvalidUserDataException(sberr.toString())
+        }
+    }
+}
---
+++ src/main/groovy/ws/antonov/gradle/plugins/protobuf/ProtobufPlugin.groovy
@@ -0,0 +1,76 @@
+package ws.antonov.gradle.plugins.protobuf
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+import org.gradle.api.Task
+import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.compile.AbstractCompile
+import org.gradle.api.internal.ConventionMapping
+import org.gradle.api.tasks.ConventionValue
+import org.gradle.api.plugins.Convention
+import org.gradle.api.internal.IConventionAware
+import org.gradle.api.internal.file.DefaultSourceDirectorySet
+
+class ProtobufPlugin implements Plugin<Project> {
+    void apply(final Project project) {
+        //println "*** --- Initializing ProtobufPlugin --- ***"
+        project.apply plugin: 'base' // We apply the base plugin to have the clean<taskname> rule
+        project.apply plugin: 'java'
+
+        project.convention.plugins.protobuf = new ProtobufConvention();
+
+        project.convention.plugins.java.sourceSets.each {
+            def srcSetName = it.name
+            it.java.srcDir("${project.buildDir}/proto-generated/${srcSetName}")
+            //println "${project.buildDir}/proto-generated/${srcSetName}"
+
+            def compileProtobufTaskName = it.getCompileTaskName("proto")
+            //println compileProtobufTaskName
+            
+            ProtobufCompile compileProtobufTask = project.tasks.add(compileProtobufTaskName, ProtobufCompile.class);
+
+            String compileJavaTaskName = it.getCompileTaskName("java");
+            //println compileJavaTaskName
+            
+            Task compileJavaTask = project.tasks.getByName(compileJavaTaskName);
+            compileJavaTask.dependsOn(compileProtobufTask)
+            compileProtobufTask.description = "Compiles the ${srcSetName} Protobuf source."
+
+            configureProtoc(project, compileProtobufTask)
+            configureForSourceSet(project, it, compileProtobufTask)
+        }
+        //println "*** --- Done Initializing ProtobufPlugin --- ***\n\n"
+    }
+
+    void configureProtoc(final project, compile) {
+        def conventionMapping = compile.getConventionMapping();
+        conventionMapping.map("protocPath", new ConventionValue() {
+            public Object getValue(Convention convention, IConventionAware conventionAwareObject) {
+                return project.convention.plugins.protobuf.protocPath
+            }
+        });
+    }
+
+    void configureForSourceSet(final project, final sourceSet, compile) {
+        def conventionMapping = compile.getConventionMapping();
+        conventionMapping.map("classpath", new ConventionValue() {
+            public Object getValue(Convention convention, IConventionAware conventionAwareObject) {
+                return sourceSet.getCompileClasspath();
+            }
+        });
+        conventionMapping.map("defaultSource", new ConventionValue() {
+            public Object getValue(Convention convention, IConventionAware conventionAwareObject) {
+                def defaultSource = new DefaultSourceDirectorySet("${sourceSet.displayName} Protobuf source", project.fileResolver);
+                defaultSource.include("**/*.proto")
+                defaultSource.filter.include("**/*.proto")
+                defaultSource.srcDir("src/${sourceSet.name}/proto")
+                return defaultSource
+            }
+        });
+        conventionMapping.map("destinationDir", new ConventionValue() {
+            public Object getValue(Convention convention, IConventionAware conventionAwareObject) {
+                return new File("${project.buildDir}/proto-generated/${sourceSet.name}")
+            }
+        });
+    }
+}
