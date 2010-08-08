b299343fbdeef215068b0353b5a52ee0debc8aeb
Alex Antonov <alex@antonov.ws>
2010-08-08 18:55:24 +0000

    Moved code into gradle-plugin-protobuf to enable separation between repo and code trunk

---
+++ src/main/groovy/ws/antonov/gradle/plugins/protobuf/ProtobufConvention.groovy
@@ -0,0 +1,11 @@
+package ws.antonov.gradle.plugins.protobuf
+
+import org.gradle.api.Project
+
+class ProtobufConvention {
+    def ProtobufConvention(Project project) {
+        
+    }
+
+    def String protocPath = "protoc"
+}
