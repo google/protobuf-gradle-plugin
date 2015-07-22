47d1430e9980ff3591cf7230f6c124a12e4f491b
Kun Zhang <zhangkun@google.com>
2015-07-22 09:36:28 -0700

    Delete a checked-in generated file in testProjectAndroid.

--- testProjectAndroid/src/androidTest/java/io/grpc/helloworldexample/TestLibrary.java
+++ testProjectAndroid/src/androidTest/java/io/grpc/helloworldexample/TestLibrary.java
@@ -2,10 +2,10 @@ package io.grpc.helloworldexample;
 
 public class TestLibrary {
   // From src/test/proto/sample.proto
-  com.example.tutorial.Msg msg;
+  com.example.tutorial.OuterSample.Msg msg;
 
   // From src/main/proto/helloworld.proto
-  HelloRequest request;
+  Helloworld.HelloRequest request;
 
   TestLibrary() {
   }
