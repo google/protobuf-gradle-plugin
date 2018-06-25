f3ee6daab32866c206e8da1aca8c9bb367c6ac9c
zpencer <spencerfang@google.com>
2018-06-25 09:46:07 -0700

    Register unitTest generated sources with android studio (#234)
    This PR makes non emulator unit test proto outputs be added to
    the IDE.
    
    Still absent are the outputs from cross sub-project protos, but
    we do not have a stable API to traverse the task graph and solve
    this problem.

--- src/main/groovy/com/google/protobuf/gradle/ProtobufPlugin.groovy
+++ src/main/groovy/com/google/protobuf/gradle/ProtobufPlugin.groovy
@@ -465,21 +465,49 @@ class ProtobufPlugin implements Plugin<Project> {
      * Adds proto sources and generated sources to supported IDE plugins.
      */
     private void addSourcesToIde() {
-      // Make the proto source dirs known to IDEs
-      getSourceSets().each { sourceSet ->
-        ProtobufSourceDirectorySet protoSrcDirSet = sourceSet.proto
-        protoSrcDirSet.srcDirs.each { File protoDir ->
-          Utils.addToIdeSources(project, Utils.isTest(sourceSet.name), protoDir)
+      // The generated javalite sources have lint issues. This is fixed upstream but
+      // there is still no release with the fix yet.
+      //   https://github.com/google/protobuf/pull/2823
+      if (Utils.isAndroidProject(project)) {
+        // variant.registerJavaGeneratingTask called earlier already registers the generated
+        // sources for normal variants, but unit test variants work differently and do not
+        // use registerJavaGeneratingTask. Let's call addJavaSourceFoldersToModel for all tasks
+        // to ensure all variants (including unit test variants) have sources registered.
+        project.tasks.withType(GenerateProtoTask).each { GenerateProtoTask generateProtoTask ->
+          generateProtoTask.getOutputSourceDirectorySet().srcDirs.each { File outputDir ->
+            generateProtoTask.variant.addJavaSourceFoldersToModel(outputDir)
+          }
         }
-      }
-      // Make the extracted proto dirs known to IDEs
-      project.tasks.withType(ProtobufExtract).each { ProtobufExtract extractProtoTask ->
-        Utils.addToIdeSources(project, extractProtoTask.isTest, extractProtoTask.destDir)
-      }
-      // Make the generated code dirs known to IDEs
-      project.tasks.withType(GenerateProtoTask).each {  GenerateProtoTask generateProtoTask ->
-        generateProtoTask.getOutputSourceDirectorySet().srcDirs.each { File outputDir ->
-          Utils.addToIdeSources(project, generateProtoTask.isTest, outputDir)
+
+        // TODO(zpencer): add gen sources from cross project GenerateProtoTasks
+        // This is an uncommon project set up but it is possible.
+        // We must avoid using private android APIs to find subprojects that the variant depends
+        // on, such as by walking through
+        //   variant.variantData.variantDependency.compileConfiguration.allDependencies
+        // Gradle.getTaskGraph().getDependencies() should allow us to walk the task graph,
+        // but unfortunately that API is @Incubating. We can revisit it when it becomes stable.
+        // https://docs.gradle.org/4.8/javadoc/org/gradle/api/execution/
+        // TaskExecutionGraph.html#getDependencies-org.gradle.api.Task-
+
+        // TODO(zpencer): find a way to make android studio aware of the .proto files
+        // Simply adding the .proto dirs via addJavaSourceFoldersToModel does not seem to work.
+      } else {
+        // Make the proto source dirs known to IDEs
+        getSourceSets().each { sourceSet ->
+          ProtobufSourceDirectorySet protoSrcDirSet = sourceSet.proto
+          protoSrcDirSet.srcDirs.each { File protoDir ->
+            Utils.addToIdeSources(project, Utils.isTest(sourceSet.name), protoDir)
+          }
+        }
+        // Make the extracted proto dirs known to IDEs
+        project.tasks.withType(ProtobufExtract).each { ProtobufExtract extractProtoTask ->
+          Utils.addToIdeSources(project, extractProtoTask.isTest, extractProtoTask.destDir)
+        }
+        // Make the generated code dirs known to IDEs
+        project.tasks.withType(GenerateProtoTask).each { GenerateProtoTask generateProtoTask ->
+          generateProtoTask.getOutputSourceDirectorySet().srcDirs.each { File outputDir ->
+            Utils.addToIdeSources(project, generateProtoTask.isTest, outputDir)
+          }
         }
       }
     }
