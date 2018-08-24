4de07b1401434ae277b61907e9ae024004c04ceb
Marco Ferrer <35935108+marcoferrer@users.noreply.github.com>
2018-08-23 23:22:15 -0400

    added kotlin dsl example

---
+++ exampleProject/build.gradle.kts
@@ -0,0 +1,62 @@
+apply(plugin = "java")
+apply(plugin = "com.google.protobuf")
+apply(plugin = "idea")
+
+repositories {
+    maven("https://plugins.gradle.org/m2/" )
+}
+
+buildscript {
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.7-SNAPSHOT")
+    }
+}
+
+dependencies {
+    compile("com.google.protobuf:protobuf-java:3.0.0")
+    compile("io.grpc:grpc-stub:1.0.0-pre2")
+    compile("io.grpc:grpc-protobuf:1.0.0-pre2")
+    if (JavaVersion.current().isJava9Compatible) {
+        // Workaround for @javax.annotation.Generated
+        // see: https://github.com/grpc/grpc-java/issues/3633
+        compile("javax.annotation:javax.annotation-api:1.3.1")
+    }
+    // Extra proto source files besides the ones residing under
+    // "src/main".
+    "protobuf"(files("lib/protos.tar.gz"))
+    "protobuf"(fileTree("ext/"))
+
+    testCompile("junit:junit:4.12")
+    // Extra proto source files for test besides the ones residing under
+    // "src/test".
+    "testProtobuf"(files("lib/protos-test.tar.gz"))
+}
+
+protobuf {
+    protoc {
+        // The artifact spec for the Protobuf Compiler
+        artifact = "com.google.protobuf:protoc:3.0.0"
+    }
+    plugins {
+        // Optional: an artifact spec for a protoc plugin, with "grpc" as
+        // the identifier, which can be referred to in the "plugins"
+        // container of the "generateProtoTasks" closure.
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.0.0-pre2"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach{
+            it.plugins {
+                // Apply the "grpc" plugin whose spec is defined above, without
+                // options.  Note the braces cannot be omitted, otherwise the
+                // plugin will not be added. This is because of the implicit way
+                // NamedDomainObjectContainer binds the methods.
+                id("grpc")
+            }
+        }
+    }
+}
