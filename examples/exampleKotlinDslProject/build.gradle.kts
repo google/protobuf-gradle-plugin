cdae1bd5db008b1921f1dd87085f1d534dc63e7a
Marco Ferrer <35935108+marcoferrer@users.noreply.github.com>
2018-10-12 17:17:01 -0400

    update example script with custom configuration dependency

--- examples/exampleKotlinDslProject/build.gradle.kts
+++ examples/exampleKotlinDslProject/build.gradle.kts
@@ -73,6 +73,10 @@ dependencies {
     protobuf(files("lib/protos.tar.gz"))
     protobuf(fileTree("ext/"))
 
+    // Adding dependency for configuration from custom sourceSet
+    // Supports all common dependency notations. ie. protobuf["sample"]("com.google.protobuf:protobuf-java:3.6.1")
+    protobuf["sample"](files("lib/protos.tar.gz"))
+
     testCompile("junit:junit:4.12")
     // Extra proto source files for test besides the ones residing under
     // "src/test".
