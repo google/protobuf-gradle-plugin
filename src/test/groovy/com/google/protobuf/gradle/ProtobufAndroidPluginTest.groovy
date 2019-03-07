59c3897822928b319a5ea93d33e13d341fe2ef24
Kun Zhang <zhangkun@google.com>
2019-03-06 17:48:38 -0800

    Do not use deprecated Android Plugin API when new API is available

--- src/test/groovy/com/google/protobuf/gradle/ProtobufAndroidPluginTest.groovy
+++ src/test/groovy/com/google/protobuf/gradle/ProtobufAndroidPluginTest.groovy
@@ -11,8 +11,8 @@ import spock.lang.Specification
  */
 class ProtobufAndroidPluginTest extends Specification {
   // TODO(zhangkun83): restore 3.0/2.2.0 once https://github.com/gradle/gradle/issues/8158 is resolved
-  private static final List<String> GRADLE_VERSION = [/* "3.0", */ "4.2", "4.3", "5.1"]
-  private static final List<String> ANDROID_PLUGIN_VERSION = [/* "2.2.0", */ "2.3.0", "2.3.0", "3.1.0"]
+  private static final List<String> GRADLE_VERSION = [/* "3.1", */ "4.3", "5.1"]
+  private static final List<String> ANDROID_PLUGIN_VERSION = [/* "2.2.0", */ "2.3.0", "3.1.0"]
 
   void "testProjectAndroid should be successfully executed (java only)"() {
     given: "project from testProject, testProjectLite & testProjectAndroid"
