b0f968b378933bea005ffef019688d6b349e25ca
Chengyuan Zhang <chengyuanzhang@google.com>
2019-10-17 00:49:03 -0700

    Upgrade Gradle/Android plugin versions being covered in tests

--- src/test/groovy/com/google/protobuf/gradle/ProtobufJavaPluginTest.groovy
+++ src/test/groovy/com/google/protobuf/gradle/ProtobufJavaPluginTest.groovy
@@ -13,7 +13,7 @@ import spock.lang.Specification
  */
 class ProtobufJavaPluginTest extends Specification {
   // TODO(zhangkun83): restore 3.0 once https://github.com/gradle/gradle/issues/8158 is resolved
-  private static final List<String> GRADLE_VERSIONS = [/* "3.0",*/ "4.0", "4.3", "5.1"]
+  private static final List<String> GRADLE_VERSIONS = [/* "3.0",*/ "5.0", "5.1", "5.4", "5.6"]
 
   void "testApplying java and com.google.protobuf adds corresponding task to project"() {
     given: "a basic project with java and com.google.protobuf"
