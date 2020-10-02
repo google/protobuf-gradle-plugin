6dbd5bd31d2feebc052caee754348be5fcda47b8
Chengyuan Zhang <chengyuanzhang@google.com>
2020-10-02 11:48:30 -0700

    Revert "Support packaging proto files into android archives(aar) (#414)" (#437)
    This reverts commit 15607b4c41199e76d417fb5cb7085c10f74dcf97.

--- src/main/groovy/com/google/protobuf/gradle/ProtobufPlugin.groovy
+++ src/main/groovy/com/google/protobuf/gradle/ProtobufPlugin.groovy
@@ -220,11 +220,6 @@ class ProtobufPlugin implements Plugin<Project> {
      */
     private void addProtoTasks() {
       if (Utils.isAndroidProject(project)) {
-        // Avoid packaging proto files into apk.
-        if (!project.android.hasProperty('libraryVariants')) {
-          project.android.packagingOptions.exclude("**/*.proto")
-        }
-
         getNonTestVariants().each { variant ->
           addTasksForVariant(variant, false)
         }
@@ -276,8 +271,6 @@ class ProtobufPlugin implements Plugin<Project> {
 
       variant.sourceSets.each {
         setupExtractProtosTask(generateProtoTask, it.name)
-        // Add proto files to java resources, and package them into aar/classes.jar.
-        it.resources.srcDirs += generateProtoTask.sourceFiles
       }
 
       if (variant.hasProperty("compileConfiguration")) {
