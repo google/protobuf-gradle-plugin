5466f25a7561c54cbf6290acc78ca28ae96b7a11
rougsig <rougsig@gmail.com>
2022-07-11 22:51:42 +0300

    Add empty TaskRegistrationTest

---
+++ src/test/groovy/com/google/protobuf/gradle/TaskRegistrationTest.groovy
@@ -0,0 +1,10 @@
+package com.google.protobuf.gradle
+
+import groovy.transform.CompileDynamic
+
+/**
+ * Unit tests for task registration functionality
+ */
+@CompileDynamic
+class TaskRegistrationTest {
+}
