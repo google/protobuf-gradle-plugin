1ad82897382e452fe83b4b2e87ccccf4ab270003
Andrey Stepankov <rougsig@gmail.com>
2022-08-17 04:45:39 +0300

    Fix examples (#592)

--- examples/exampleKotlinDslProject/settings.gradle.kts
+++ examples/exampleKotlinDslProject/settings.gradle.kts
@@ -1,18 +1,1 @@
-pluginManagement{
-    repositories {
-        mavenLocal()
-        maven("https://plugins.gradle.org/m2/")
-    }
-    // The resolutionStrategy only needs to be configured for 
-    // local plugin development, specifically when using the 
-    // mavenLocal repository.
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "com.google.protobuf") {
-                useModule("com.google.protobuf:protobuf-gradle-plugin:${requested.version}")
-            }
-        }
-    }
-}
 rootProject.name = "exampleProject"
-
