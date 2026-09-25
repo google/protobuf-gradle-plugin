de84efbbc0a399a7f578cd0dfe7eb6ac223d2d76
John Burns <wakingrufus@users.noreply.github.com>
2026-09-25 15:50:49 -0500

    Set CATEGORY_ATTRIBUTE on compileProtoPath configurations (#813)
    This builds on 44309a207 by also specifying Category to avoid ambiguous
    variant selection errors.
    
    Fixes #794

--- src/main/groovy/com/google/protobuf/gradle/ProtobufPlugin.groovy
+++ src/main/groovy/com/google/protobuf/gradle/ProtobufPlugin.groovy
@@ -41,6 +41,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.artifacts.Configuration
+import org.gradle.api.attributes.Category
 import org.gradle.api.attributes.LibraryElements
 import org.gradle.api.attributes.Usage
 import org.gradle.api.file.ConfigurableFileCollection
@@ -204,6 +205,9 @@ class ProtobufPlugin implements Plugin<Project> {
                 .attribute(
                         Usage.USAGE_ATTRIBUTE,
                         project.getObjects().named(Usage, Usage.JAVA_RUNTIME))
+                .attribute(
+                        Category.CATEGORY_ATTRIBUTE,
+                        project.getObjects().named(Category, Category.LIBRARY))
       }
     }
 
